0f5d7af9b971db3dfc70ead5044f89d4c429ca71
贾维斯 <claw@abab.pw>
2026-06-22 22:10:19 +0800

    chore: remove hardcoded secrets and use dotenv

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
 
-APP_ID = "***REDACTED_APPID***"
-APP_SECRET = "***REDACTED***"
+load_dotenv()
+
+APP_ID = os.getenv("FEISHU_APP_ID", "")
+APP_SECRET = os.getenv("FEISHU_APP_SECRET", "")
 
 SESSION_FILE = "chat_sessions.json"
 PROFILE_FILE = "user_profiles.json"
